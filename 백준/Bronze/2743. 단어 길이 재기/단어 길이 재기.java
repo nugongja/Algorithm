08790f9c391af40242c9a60ae3458e0e7d4abd4a
nugongja <nugongja@gmail.com>
2025-03-08 17:56:42 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.length());
+
+
+    }
+}
