c52152f2a766797ba700c5ed1816a432a6454bd4
nugongja <nugongja@gmail.com>
2025-03-11 01:00:59 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.nextLine().charAt(0);
+
+        System.out.println((int)c);
+
+
+    }
+}
