a6bed3e97a824f4afc6657e29c745fdc966f84cb
nugongja <nugongja@gmail.com>
2025-03-30 14:34:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,28 +1,12 @@
 def solution(n, arr1, arr2):
     answer = []
 
-    tmp = [[' ' for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        code = arr1[i]
 
-        j = 0
-        while j < n:
-            if code%2 == 1:
-                tmp[i][n-j-1] = '#'
-            code //= 2
-            j+=1
+    for i, j in zip(arr1, arr2):
+        tmp = (bin(i|j)[2:]).rjust(n, '0').replace('1','#').replace('0', ' ')
+        answer.append(tmp)
+        
 
-        code = arr2[i]
 
-        j = 0
-        while j < n:
-            if code%2 == 1:
-                tmp[i][n-j-1] = '#'
-            code //= 2
-            j+=1
-
-    for i in range(n):
-        answer.append("".join(tmp[i]))
     
-
     return answer
