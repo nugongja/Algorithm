fa8dda296148f96f55fddec71ca63bbbd3bd4124
nugongja <nugongja@gmail.com>
2025-04-20 20:23:22 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 168 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        Long n = sc.nextLong();
+
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
