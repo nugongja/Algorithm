88458f54bc7f04dcf294f6b750d68597f3710dc2
nugongja <nugongja@gmail.com>
2025-06-26 15:46:24 +0900

    [Silver V] Title: 단어 정렬, Time: 60 ms, Memory: 7208 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <queue>
 #include <string>
-#include <set>
+#include <unordered_set>
 using namespace std;
 
 int main() {
 	int N;
 	string tmp;
-	priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> pq;
-	set<string> seen;
+	priority_queue<pair<int, string>, vector<pair<int, string>>, greater<>> pq;
+	unordered_set<string> seen;
 
 	cin >> N;
 	cin.ignore();
 
 	while (N-- > 0) {
 		getline(cin, tmp);
-		if (seen.find(tmp) != seen.end()) {
-			continue; // Skip duplicates
-		}
-		pq.push(make_pair(tmp.size(), tmp));
+		if (seen.count(tmp)) continue; 
 		seen.insert(tmp);
+		pq.push({static_cast<int>(tmp.size()), tmp});
 	}
 
 	while (!pq.empty()) {
