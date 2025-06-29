9074254969eeb5ae5ef30760b18e22e3bcf11a40
nugongja <nugongja@gmail.com>
2025-06-29 19:10:22 +0900

    [Silver V] Title: 숫자 카드, Time: 360 ms, Memory: 23304 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,62 +1,34 @@
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <queue>
+#include <unordered_set>
 using namespace std;
 
 int main() {
-    int N;
-    int M;
-    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N,M, tmp;
+
     cin >> N;
-    cin.ignore();
-
-
-    priority_queue<int, vector<int>, greater<int>> card;
-    int tmp;
+    unordered_set<int> card;
     for (int i = 0; i < N; i++) {
         cin >> tmp;
-        card.push(tmp);
+        card.insert(tmp);
     }
-
-    cin.ignore();
 
 
     cin >> M;
-    cin.ignore();
-    
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> arr;
     for (int i = 0; i < M; i++) {
         cin >> tmp;
-        arr.push({ tmp, i });
-    }
-
-    int* value = new int[M];
-    while(!arr.empty()) {
-
-        if (card.empty()) {
-            value[arr.top().second] = 0;
-            arr.pop();
-            continue;
-        }
-
-        if (card.top() < arr.top().first) {
-            card.pop();
-        }
-        else if (card.top() == arr.top().first) {
-            card.pop();
-            value[arr.top().second] = 1;
-            arr.pop();
+        if (card.count(tmp) > 0) {
+            cout << "1 ";
         }
         else {
-            value[arr.top().second] = 0;
-            arr.pop();
+            cout << "0 ";
         }
     }
 
-    for (int i = 0; i < M; i++) {
-        cout << value[i] << " ";
-    }
+    
+    
 
     return 0;
 }
