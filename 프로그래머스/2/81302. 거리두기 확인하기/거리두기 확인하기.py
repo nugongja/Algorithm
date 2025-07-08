753001f7696d9bcb4bd23d6c82988d9188bce5b8
nugongja <nugongja@gmail.com>
2025-07-09 01:54:59 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.13 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,51 +1,38 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
+
+def is_safe(room):
+    for i in range(5):
+        for j in range(5):
+            if room[i][j] != 'P':
+                continue
+            q = deque()
+            q.append((i, j, 0))
+            visited = [[False]*5 for _ in range(5)]
+            visited[i][j] = True
+            
+            while q:
+                x, y, dist = q.popleft()
+                if dist >= 3:
+                    continue
+                
+                for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+                    nx, ny = x+dx, y+dy
+                    if 0 <= nx < 5 and 0 <= ny < 5 and not visited[nx][ny]:
+                        visited[nx][ny] = True
+                        if room[nx][ny] == 'P' and dist < 2:
+                            return 0
+                        if room[nx][ny] == 'O':
+                            q.append((nx, ny, dist+1))
+    return 1
 
 def solution(places):
     answer = []
 
-    idx = 0
     for room in places:
-        idx += 1
-        Person = []
-        for i in range(5):
-            for j in range(5):
-                if room[i][j] == 'P':
-                    Person.append((i,j))
-        
-        isPossible = True
-        for i in range(len(Person)-1):
-            r1 = Person[i][0]
-            c1 = Person[i][1]
-            for j in range(i+1, len(Person)):
-                r2 = Person[j][0]
-                c2 = Person[j][1]
-
-                manhaten = abs(c1-c2) + abs(r1-r2)
-
-                if manhaten == 1: 
-                    isPossible = False
-                    break
-                elif manhaten == 2:
-                    if r1 == r2 or c1 == c2:
-                        tmp_r = (r1+r2)//2
-                        tmp_c = (c1+c2)//2
-                        if room[tmp_r][tmp_c] == 'O':
-                            isPossible = False
-                            break
-                    else:
-                        if room[r1][c2] == 'O' or room[r2][c1] == 'O':
-                                isPossible = False
-                                break
-            if not isPossible:
-                break
-                
-        if isPossible:
-            answer.append(1)
-        else:
-            answer.append(0)
-
-                    
+        answer.append(is_safe(room))
+                        
             
 
     return answer
