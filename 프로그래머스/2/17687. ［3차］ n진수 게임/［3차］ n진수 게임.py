de54343973ac9e4291f8cd0119fa15e697dd2720
nugongja <nugongja@gmail.com>
2025-07-15 16:09:10 +0900

    [level 2] Title: [3차] n진수 게임, Time: 19.28 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,14 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-def solution(n, t, m, p):
-
-    def tenToN(i, n):
-        tmp = ''
+def tenToN(i, n):
+        characters = '0123456789ABCDEF'
 
         if i == 0:
             return '0'
 
+        tmp = ''
         while i > 0:
             tmp = characters[i%n] + tmp
             i //= n
@@ -16,22 +15,15 @@ def solution(n, t, m, p):
         return tmp
 
 
-    characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
+def solution(n, t, m, p):
 
-    string = ''
+    s = ''
     i = 0
-    while len(string) < p+(t-1)*m:
-        temp = tenToN(i, n)
-        string += temp
-        i += 1
+    # # 튜브가 말해야 할 위치들을 안전하게 커버함 (총 t개를 말하고, 말할 위치는 m 간격으로 점프)
+    while len(s) < t*m:  
+         s += tenToN(i, n)
+         i += 1
 
-
-    answer = ''
-    cnt = 0
-    while cnt != t:
-        answer += string[(p-1)+cnt*m]
-        cnt += 1
-   
-
-    return answer
+    # p-1: 튜브의 시작 인덱스, m: 한 바퀴마다 돌아오는 순서 간격, t: 말해야 할 횟수
+    return s[p-1::m][:t]  
